9ae52b1c5f020e4eec73c72ae06fb9593f73f8dd
AnannyaDeshpand-054 <anannyamd1809@gmail.com>
2026-01-28 18:21:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 // Home route
 app.get('/', (req, res) => {
-  res.send('Node.js Backend CI/CD Demo is working smoothly !!🚀');
+  res.send('Node.js Backend  Demo is working smoothly !!🚀');
 });
 
 // Health check route
